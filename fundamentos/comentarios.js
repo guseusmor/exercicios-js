2f59e4222e05a181c7389838bdf12ae60b258c7d
Gustavo Eustáquio <gustavo@arpiatecnologia.com.br>
2020-02-04 11:56:46 -0300

    Primeiro commit

---
+++ fundamentos/comentarios.js
@@ -0,0 +1,17 @@
+/*
+Comentando meu código
+Primeira maneira de comentar o código
+*/
+
+/**
+ * Segunda maneira de comentar o código
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+ //Terceira forma de comentar o código
+
+
